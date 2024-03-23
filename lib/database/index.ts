5d40ca308f0635adcf0cfd37cb9a5ad876318539
Archan <archanboseofficial@gmail.com>
2024-03-23 16:21:36 +0530

    Refactor webhook endpoint and database connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -3,9 +3,8 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose ||{ conn: null, promise: null };
 export const connectToDatabase = async () => {
-    if(cached.conn){
-        return cached.conn;
-    }
+    if(cached.conn) return cached.conn;
+    
     if(!MONGODB_URI){
         throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
     }
